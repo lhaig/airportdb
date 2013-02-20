38472e05006f8040356d59f99ce749522c63d5b4
Lance Haig <lhaig@haigmail.com>
2013-02-20 11:01:19 +0200

    Start From Scratch

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Airportdb::Application.config.secret_token = '40ff9ed823882ecac72367f8c451b645e534ea10464d2183f5dfb3184080fe03f2cb17ba124df41683c6e9f178eb413a5201a02696ab3ae8ff3e8514920ae071'
+Airportdb::Application.config.secret_token = '97f324db93fd782fff8339a3c43b9d2f7dc9d49a960a6f6b54f11885e196e29542e55390055234df3f23fb3a001a85822924043be93344cfd6f422f56ac1e07c'
--- test/unit/helpers/airports_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class AirportsHelperTest < ActionView::TestCase
-end
