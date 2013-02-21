70cd907760092cc8949a59ef4824d21e73ab0b79
Lance Haig <lhaig@haigmail.com>
2013-02-21 11:49:52 +0200

    More assosiation changes

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,5 +1,5 @@
 class Airport < ActiveRecord::Base
   attr_accessible :continent, :country_id, :elevation_ft, :gps_code, :home_link, :iata_code, :ident, :keywords, :latitude_deg, :local_code, :longitude_deg, :municipality, :name, :region_id, :scheduled_service, :type, :wikipedia_link
-  has_and_belongs_to_many :countries
-  has_and_belongs_to_many :regions
+  has_one :country
+  has_one :region
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,5 +1,5 @@
 class Region < ActiveRecord::Base
-  attr_accessible :code, :continent, :country_id, :keywords, :local_code, :name, :wikipedia_link
-  has_many :countries
+  attr_accessible :code, :continent_id, :country_id, :keywords, :local_code, :name, :wikipedia_link
+  has_and_belongs_to_many :countries
   has_and_belongs_to_many :airports
 end
